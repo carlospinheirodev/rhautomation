a5fd663b7a917e695cae316ed32dca0a3b7d4456
carlospinheirodev <carlospinheiro.dev@gmail.com>
2025-08-04 14:39:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 from routes import *
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        host='0.0.0.0',  # IMPORTANTE: não use 'localhost' ou '127.0.0.1'
+        port=int(os.environ.get('PORT', 5000)),
+        debug=os.environ.get('FLASK_ENV') == 'development'
+    )
